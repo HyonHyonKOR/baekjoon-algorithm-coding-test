fcc6fdd48f1d6ff564068a06db125455b1ca9374
HyonHyon <khm3626@gmail.com>
2023-11-11 16:48:58 +0900

    [Bronze V] Title: 개수 세기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,10 +1,4 @@
 length = int(input())
-arr = list(map(int,input().split()))
+length_arr = list(map(int,input().split()))
 find_num = int(input())
-
-count = 0 
-for i in range(length):
-    if arr[i] == find_num:
-        count += 1
-
-print(count)
+print(length_arr.count(find_num))
