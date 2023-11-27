3956cda92c3e63591ab3ba0e58cf043d9870404e
HyonHyon <khm3626@gmail.com>
2023-11-27 23:58:43 +0900

    [Bronze I] Title: 암호 키, Time: 1468 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,10 @@
+tc = int(input())
+
+for _ in range(tc):
+    S = int(input())
+    for i in range(2,1000001):
+        if S % i == 0:
+            print("NO")
+            break
+        if i == 1000000:
+            print("YES")
